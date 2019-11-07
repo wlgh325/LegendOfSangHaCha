9345d8155ce67d735650301b999a331f24798ba0
jiho <wlgh325@naver.com>
2019-11-07 19:04:21 +0900

    commit assets

---
+++ Code/GameProject/Assets/Scripts/UI/FirstSceneMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class FirstSceneMenu : MonoBehaviour
+{
+    public Image fadeImage;
+
+    float totalFadeTime = 1;
+
+    public Color destColor;
+    Color oriColor;
+
+    // Start is called before the first frame update
+    void Start(){
+        oriColor = fadeImage.color;    
+    }
+
+    public void Fade(){
+        StartCoroutine(Fade_());
+    }
+
+    IEnumerator Fade_(){
+        float curTime = 0;
+
+        fadeImage.gameObject.SetActive(true);
+        while(curTime < totalFadeTime){
+            curTime += Time.deltaTime;
+            fadeImage.color = Color.Lerp(oriColor, destColor, curTime);
+            yield return null;
+        }
+        ClickStartBtn();
+    }
+
+    private void ClickStartBtn(){
+        SceneManager.LoadScene("LobyScene");
+    }
+
+    public void ClickManualBtn(){
+        SceneManager.LoadScene("ManualScene");
+    }
+}
