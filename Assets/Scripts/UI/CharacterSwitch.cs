85c61401696c56ae21cd7886079e25f555349b89
jiho <wlgh325@naver.com>
2019-11-18 22:48:46 +0900

    finish control button

---
+++ Assets/Scripts/UI/CharacterSwitch.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSwitch : MonoBehaviour{
+    public Button leftBtn, rightBtn;
+    public List<Transform> characterBuffer;
+
+    bool updateUI;
+
+    int currentIndex;
+
+    // Start is called before the first frame update
+    void Start(){
+        UpdateUI();
+        UpdateCharacter();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(updateUI){
+            updateUI = false;
+            UpdateUI();
+        }
+    }
+
+    void UpdateCharacter(){
+        for (int i = 0; i < characterBuffer.Count; i++){
+            characterBuffer[i].gameObject.SetActive(i == currentIndex);
+        }
+    }
+
+    void UpdateUI(){
+        // 가장 왼쪽에 있지 않은 경우 => true(버튼 활성화)
+        leftBtn.interactable = currentIndex > 0;
+
+        // 가장 오른쪽에 있지 않은 경우 => true(버튼 활성화)
+        rightBtn.interactable = currentIndex < characterBuffer.Count - 1;
+    }
+
+    public void SwitchCharacter(bool leftOrRight){
+        // leftBtn이 true면 즉 leftBtn이 눌렸으면 -1 아니면 +1
+        currentIndex += leftOrRight ? -1 : 1;
+
+        updateUI = true;
+        UpdateCharacter();
+    }
+}
---
+++ Assets/Scripts/UI/ResourcesController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ResourcesController : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+
+    int number;
+    bool updateUI;
+    bool pointerDown;
+
+    // Start is called before the first frame update
+    void Start(){
+        
+        number = 0;
+
+        // 시작시 초기화
+        UpdateUI();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(updateUI){
+            updateUI = false;
+            UpdateUI();
+        }
+
+        if(pointerDown){
+            IncreaseNumber();
+        }
+    }
+
+    void UpdateUI(){
+        text.text = number.ToString();
+    }
+    
+    void IncreaseNumber(){
+        number += 50;
+        updateUI = true;
+    }
+
+    public void PointerDown(){
+        pointerDown = true;
+    }
+
+    public void PointerUp(){
+        pointerDown = false;
+    }
+}
