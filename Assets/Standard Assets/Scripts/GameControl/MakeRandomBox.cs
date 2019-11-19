360b7384e5ab6b5917d1b66bf3145799aa132d6b
youngjae <dudwo1344@gmail.com>
2019-11-19 14:41:12 +0900

    create boxes

---
+++ Assets/Standard Assets/Scripts/GameControl/MakeRandomBox.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeRandomBox : MonoBehaviour
+{
+    public GameObject[] boxes;
+    private GameObject instance;
+
+    void Start(){
+
+        makeRandomBox();
+    }
+
+    void makeRandomBox(){
+        int i = Random.Range(0, boxes.Length);
+        instance = Instantiate(boxes[i], transform.position, Quaternion.identity) as GameObject;
+    }
+}
